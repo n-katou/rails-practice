c81779a8077ddec00ca29da1e08dd2366d27288a
n-katou <102382177+n-katou@users.noreply.github.com>
2024-02-01 14:14:41 +0900

    board create

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,23 @@
 class BoardsController < ApplicationController
   def index
+    @boards = Board.all
+  end
+
+  def new
+    @board = Board.new
+    # binding.pry
+  end
+
+  def create
+    Board.create(board_params)
+    #binding.pry
   end
   
+  private
+  
+  def board_params
+    params.require(:board).permit(:author_name, :title, :body)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "boards#index"
+  # Defines the root path route ("/") 
+  get 'boards', to: 'boards#index'
+  get 'boards/new', to: 'boards#new'
+  post 'boards/', to:'boards#create'
 end
